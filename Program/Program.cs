f18705097666fc8753b138b50bbb1b91a4344c6f
floxxui <60339661+floxxui@users.noreply.github.com>
2020-09-24 18:16:59 +0200

    Epic commit :P

--- Program/Program.cs
+++ Program/Program.cs
@@ -20,14 +20,27 @@ namespace Program
 
             for (int i = 0; i < 3; i++)
             {
-                p1Location[i] = GetLocation();
+                p1Location[i] = GetLocation(i);
             }
 
+            p1.x = p1Location[0];
+            p1.y = p1Location[1];
+            p1.z = p1Location[2];
+
+            System.Console.WriteLine("Player 1 location: " + p1.x + ", " + p1.y + ", " + p1.z);
+            System.Console.WriteLine("Player 2 location: " + p2.x + ", " + p2.y + ", " + p2.z);
+
+            GetDistance( p1Location, p2Location);
+
+            Console.ReadLine();
         }
 
-        static int GetLocation() 
+        static int GetLocation(int i) 
         {
-            System.Console.WriteLine("Write a number for p1's location");
+            string[] whichLocation = {"x", "y", "z"};
+
+            System.Console.WriteLine("Write a number for player 1's location " + whichLocation[i]);
+
             while(true)
             {
                 string playerAnswer = Console.ReadLine();
@@ -45,6 +58,28 @@ namespace Program
                     System.Console.WriteLine("Please enter a number for p1's location");
                 }
             }
+        }
+
+        static void GetDistance(int[] p1Location, int[] p2Location)
+        {
+            int[] distance = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (p1Location[i] >= p2Location[i])
+                {
+                    distance[i] = p1Location[i] - p2Location[i];
+                }
+                else if (p1Location[i] <= p2Location[i])
+                {
+                    distance[i] = p2Location[i] - p1Location[i];
+                }
+                else
+                {
+                    distance[i] = 0;
+                } 
+            }
+            Console.WriteLine("The distance between player 1 and player 2 is " + distance[0] + ", " + distance[1] + ", " + distance[2]);
         }
     }
 }
